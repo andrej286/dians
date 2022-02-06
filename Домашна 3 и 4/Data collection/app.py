65e03e538631f619d06434f4cd5ff7762179f5a9
andrej286 <46653449+andrej286@users.noreply.github.com>
2022-02-06 01:14:18 +0100

    Add files via upload

---
+++ Домашна 3 и 4/Data collection/app.py
@@ -0,0 +1,118 @@
+import requests
+import json
+from geopy.geocoders import Nominatim
+from flask import Flask
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def index():
+    return "Hello"
+
+
+@app.route('/restaurants', methods=['GET'])
+def restaurants():
+    with open('json/restaurant_places.json') as json_file:
+        data = json.load(json_file)
+        return data
+
+
+@app.route('/fastfood', methods=['GET'])
+def fast_food():
+    with open('json/fast_food_places.json') as json_file:
+        data = json.load(json_file)
+        return data
+
+
+@app.route('/update', methods=['GET'])
+def update():
+    get_fast_food_places()
+    get_restaurant_places()
+
+
+def get_fast_food_places():  # Updates the database with fast food places. It runs by going to the /update endpoint.
+    global overpass_url, overpass_query, response, fast_food_places
+    overpass_url = "http://overpass-api.de/api/interpreter"
+    overpass_query = """
+    [out:json];
+    area["ISO3166-1"="MK"][admin_level=2];
+    (node["amenity"="fast_food"](area);
+     way["amenity"="fast_food"](area);
+     rel["amenity"="fast_food"](area);
+    );
+    out center;
+    """
+    response = requests.get(overpass_url, params={'data': overpass_query})
+    fast_food_places = response.json()['elements']
+    fast_food_dict = {}
+
+    for fast_food in fast_food_places:
+        tags = fast_food['tags']
+        if 'name' in tags:
+            locator = Nominatim(user_agent='myGeocoder')
+            coordinates = str(fast_food['lat']) + ', ' + str(fast_food['lon'])
+            location = locator.reverse(coordinates)
+            address = location.raw['address']
+            if 'city' in address:
+                city = address['city']
+                if city in fast_food_dict:
+                    fast_food_dict[city].append(fast_food)
+                else:
+                    fast_food_dict[city] = [fast_food]
+            elif 'village' in address:
+                village = address['village']
+                if village in fast_food_dict:
+                    fast_food_dict[village].append(fast_food)
+                else:
+                    fast_food_dict[village] = [fast_food]
+
+    json_file = json.dumps(fast_food_dict)
+    f = open("json/fast_food_places.json", "w")
+    f.write(json_file)
+    f.close()
+
+
+def get_restaurant_places():  # Updates the database with fast food places. It runs by going to the /update endpoint.
+    global overpass_url, overpass_query, response
+    overpass_url = "http://overpass-api.de/api/interpreter"
+    overpass_query = """
+    [out:json];
+    area["ISO3166-1"="MK"][admin_level=2];
+    (node["amenity"="restaurant"](area);
+     way["amenity"="restaurant"](area);
+     rel["amenity"="restaurant"](area);
+    );
+    out center;
+    """
+    response = requests.get(overpass_url, params={'data': overpass_query})
+    restaurants = response.json()['elements']
+    restaurant_dict = {}
+    for restaurant in restaurants:
+        tags = restaurant['tags']
+        if 'name' in tags:
+            locator = Nominatim(user_agent='myGeocoder')
+            coordinates = str(restaurant['lat']) + ', ' + str(restaurant['lon'])
+            location = locator.reverse(coordinates)
+            address = location.raw['address']
+            if 'city' in address:
+                city = address['city']
+                if city in restaurant_dict:
+                    restaurant_dict[city].append(restaurant)
+                else:
+                    restaurant_dict[city] = [restaurant]
+            elif 'village' in address:
+                village = address['village']
+                if village in restaurant_dict:
+                    restaurant_dict[village].append(restaurant)
+                else:
+                    restaurant_dict[village] = [restaurant]
+    json_file = json.dumps(restaurant_dict)
+    f = open("restaurant_places.json", "w")
+    f.write(json_file)
+    f.close()
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ Домашна 3 и 4/Website/app.py
@@ -0,0 +1,100 @@
+import requests
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.debug = True
+
+
+# Class that represents every place in the database.
+
+class Place:
+    def __init__(self, lat, lon, name):
+        self.lat = lat
+        self.lon = lon
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def get_larger_map(self):
+        return "https://www.openstreetmap.org/?mlat=" + str(self.lat) + "&mlon=" + str(self.lon) + "#map=19/" + str(
+            self.lat) + "/" + str(self.lon)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/restaurant', methods=['GET'])
+def dropdown_restaurant():
+    cities = get_cities('restaurant')
+    return render_template('restaurant.html', cities=cities)
+
+
+@app.route('/fastfood', methods=['GET'])
+def dropdown_fastfood():
+    cities = get_cities('fastfood')
+    return render_template('fastfood.html', cities=cities)
+
+
+@app.route('/restaurant/choices', methods=['GET'])
+def restaurant_places():
+    city = request.args.get('city')
+    places = get_places('restaurant', city)
+    return render_template('selected_city.html', places=places)
+
+
+@app.route('/fastfood/choices', methods=['GET'])
+def fastfood_places():
+    city = request.args.get('city')
+    places = get_places('fastfood', city)
+    return render_template('selected_city.html', places=places)
+
+
+def get_places(selection, city):  # Return all places in the selected city.
+    if selection == 'restaurant':
+        data = requests.get('https://data-collection-dians.herokuapp.com/restaurants').json()[city]
+        places = []
+
+        for place in data:
+            lat = place['lat']
+            lon = place['lon']
+            name = place['tags']['name']
+            place = Place(lat, lon, name)
+            places.append(place)
+
+        return places
+    if selection == 'fastfood':
+        data = requests.get('https://data-collection-dians.herokuapp.com/fast_food').json()[city]
+        places = []
+
+        for place in data:
+            lat = place['lat']
+            lon = place['lon']
+            name = place['tags']['name']
+            place = Place(lat, lon, name)
+            places.append(place)
+
+        return places
+
+
+def get_cities(selection):  # Get all the available cities.
+    if selection == 'restaurant':
+        data = requests.get('https://data-collection-dians.herokuapp.com/restaurants').json()
+        cities_sk = []
+
+        for key in data.keys():
+            cities_sk.append(key)
+        return cities_sk
+    if selection == 'fastfood':
+        data = requests.get('https://data-collection-dians.herokuapp.com/fast_food').json()
+        cities_sk = []
+
+        for key in data.keys():
+            cities_sk.append(key)
+        return cities_sk
+
+
+if __name__ == "__main__":
+    app.run()
